4634b2b55fc13f9f568e66ab3b562e11221dccde
Emerson Batista <84859510+emersondev21@users.noreply.github.com>
2024-02-14 19:55:15 +0000

    .

--- assets/script/scipt.js
+++ assets/script/scipt.js
@@ -1,19 +1,59 @@
 const inputHeight = document.getElementById('inpt_height')
 const inputWeight = document.getElementById('inpt_weight')
 
-
 function calcIMC(params) {
   // Altura
   let height = inputHeight.value
   // Peso
   let weight = inputWeight.value
-  console.log(weight)
-  console.log(height)
 
   let imc = weight/(height * height)
 
-  console.log(imc)
+  if (imc < 18.5) { 
+    var resultadoIMC = document.getElementById("imc");
+    var resultadoSituation = document.getElementById("situation");
+    resultadoIMC.textContent = (imc.toFixed(2) + " kg/m²");
+    resultadoSituation.textContent = "Peso Ideal";
+    resultadoIMC.style.color = "#FFC312"
+  }
 
-  var resultadoElemento = document.getElementById("imc");
-      resultadoElemento.textContent = imc.toFixed(2);
+  else if ((imc >= 18.5) && (imc <= 24.9)) { 
+    var resultadoIMC = document.getElementById("imc");
+    var resultadoSituation = document.getElementById("situation");
+    resultadoIMC.textContent = (imc.toFixed(2) + " kg/m²");
+    resultadoSituation.textContent = "Peso Ideal";
+    resultadoIMC.style.color = "#4cd137"
+  }
+
+  else if ((imc > 24.9) && (imc <= 29.9)) { 
+    var resultadoIMC = document.getElementById("imc");
+    var resultadoSituation = document.getElementById("situation");
+    resultadoIMC.textContent = (imc.toFixed(2) + " kg/m²");
+    resultadoSituation.textContent = "Acima do peso";
+    resultadoIMC.style.color = "#EA2027"
+  }
+
+  else if ((imc > 29.9) && (imc <= 34.9)) { 
+    var resultadoIMC = document.getElementById("imc");
+    var resultadoSituation = document.getElementById("situation");
+    resultadoIMC.textContent = (imc.toFixed(2) + " kg/m²");
+    resultadoSituation.textContent = "Obesidade grau I";
+    resultadoIMC.style.color = "#EA2027"
+  }
+
+  else if ((imc > 34.9) && (imc <= 39.9)) { 
+    var resultadoIMC = document.getElementById("imc");
+    var resultadoSituation = document.getElementById("situation");
+    resultadoIMC.textContent = (imc.toFixed(2) + " kg/m²");
+    resultadoSituation.textContent = "Obesidade grau II";
+    resultadoIMC.style.color = "#EA2027"
+  }
+
+  else if (imc >= 40) { 
+    var resultadoIMC = document.getElementById("imc");
+    var resultadoSituation = document.getElementById("situation");
+    resultadoIMC.textContent = (imc.toFixed(2) + " kg/m²");
+    resultadoSituation.textContent = "Obesidade grau III";
+    resultadoIMC.style.color = "#EA2027"
+  }
 }
